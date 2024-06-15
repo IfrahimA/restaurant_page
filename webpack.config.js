4254dff3013255cf97983efe17a17549b555583d
Ifrahim <147778089+IfrahimA@users.noreply.github.com>
2024-06-15 18:23:33 -0400

    Setting up html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Restaurant Page', 
+        filename: 'index.html', 
+        template: 'src/index.html'
+    })
+  ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
